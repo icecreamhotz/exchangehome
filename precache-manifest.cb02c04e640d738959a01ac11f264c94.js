13eb589e3cb74691e69b59fa85681090e49309e6
icecreamhotz <best_dota@hotmail.com>
2018-10-13 23:27:14 +0700

    Updates

---
+++ precache-manifest.cb02c04e640d738959a01ac11f264c94.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0ba86392a4083bf12fbc887d113f090c",
+    "url": "/static/media/condo4.0ba86392.jpg"
+  },
+  {
+    "revision": "8b7d92008c479f79f89aae1be6962a62",
+    "url": "/static/media/condo3.8b7d9200.jpg"
+  },
+  {
+    "revision": "48537a289749725bdd67094eb10b2c38",
+    "url": "/static/media/condo2.48537a28.jpg"
+  },
+  {
+    "revision": "24570b043b02d00afb669ed912364737",
+    "url": "/static/media/condo1.24570b04.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "688f92639e98bff5621d",
+    "url": "/static/js/main.688f9263.chunk.js"
+  },
+  {
+    "revision": "39ab2ada879b159a107c",
+    "url": "/static/js/1.39ab2ada.chunk.js"
+  },
+  {
+    "revision": "688f92639e98bff5621d",
+    "url": "/static/css/main.07d6a188.chunk.css"
+  },
+  {
+    "revision": "12d997dfbfa65c7c939e6d4103da3d20",
+    "url": "/index.html"
+  }
+];
